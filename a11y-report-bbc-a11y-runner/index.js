d87dc5be29f42dd0ee7d38fa207ad7480c0b96ff
Joseph Wynn <joseph@wildlyinaccurate.com>
2019-11-02 19:42:35 +1300

    Add logging

--- a11y-report-bbc-a11y-runner/index.js
+++ a11y-report-bbc-a11y-runner/index.js
@@ -4,11 +4,17 @@ module.exports = async function(context, url) {
   context.log("bbc-a11y runner processing URL from queue", url)
 
   const bbcA11yPath = require.resolve("bbc-a11y")
+  context.log("bbcA11yPath", bbcA11yPath)
   const bbcA11yArgs = ["--reporter", "json", url]
+  context.log("bbcA11yArgs", bbcA11yArgs)
   const bbcA11yProcess = childProcess.spawn(bbcA11yPath, bbcA11yArgs)
+  context.log("bbcA11yProcess", bbcA11yProcess)
 
   bbcA11yProcess.stderr.on("data", data => {
+    context.log("(log) bbc-a11y failed)", data)
+    context.log("(log toString) bbc-a11y failed)", data.toString("utf8"))
     context.log.error("bbc-a11y failed", data.toString("utf8"))
+    context.log("Done after error")
     context.done()
   })
 
@@ -17,10 +23,13 @@ module.exports = async function(context, url) {
       context.log("bbc-a11y returned valid JSON")
       context.binding.results = JSON.parse(data)
     } catch (e) {
+      context.log("Ignoring non-JSON output", data)
+      context.log("Ignoring non-JSON output toString", data.toString("utf8"))
       context.log.error("bbc-a11y returned invalid JSON", data.toString("utf8"))
       // Ignore non-JSON output
     }
 
+    context.log("Done after stdout")
     context.done()
   })
 }
