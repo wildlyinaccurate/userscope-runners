8dbdd7fd00814fa96dcc215c511fbe1b1076652c
Joseph Wynn <joseph@wildlyinaccurate.com>
2019-11-13 19:14:55 +1300

    Get models from userscope-data-models

--- a11y-report-bbc-a11y-runner/TestResult.js
+++ a11y-report-bbc-a11y-runner/TestResult.js
@@ -1,12 +1,4 @@
 const mongoose = require("mongoose")
-const bbcA11yResultSchema = require("./BbcA11yResult")
+const { testResultSchema } = require("userscope-data-models")
 
-/**
- * TODO: This isn't the full TestResult schema. Ideally this project and the
- * web project should share types & schemas from a common package.
- */
-const resultSchema = new mongoose.Schema({
-  bbcA11yResults: bbcA11yResultSchema
-})
-
-module.exports = mongoose.model("TestResult", resultSchema)
+module.exports = mongoose.model("TestResult", testResultSchema)
